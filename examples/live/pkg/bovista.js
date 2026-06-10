45ef7b8928dbe36d42cb5aada9873c283597ab28
aganders3 <aganders3@users.noreply.github.com>
2026-06-10 15:16:45 +0000

    deploy: ce48ef3144fae68248d9a0916cfeda05cdd91085

--- examples/live/pkg/bovista.js
+++ examples/live/pkg/bovista.js
@@ -1736,7 +1736,7 @@ function __wbg_get_imports() {
             arg0.writeTexture(arg1, arg2, arg3, arg4);
         }, arguments); },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 115, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 113, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h4e013bd767eefc9f);
             return ret;
         },
